3e397cdc6d9a53351e96d595ab42f32ec80154d2
Subash <subash@codewithsubash.com>
2021-10-28 20:30:02 +0545

    average wala

---
+++ avg.py
@@ -0,0 +1,7 @@
+a=input("Enter a first n.b")# here a is string and we have to make it
+b=input("Enter a second n.b")# it integer bcoz it's we are calculating with n.b
+a = int(a)
+b = int(b)
+avg=(a+b)/25
+
+print("avg is ", avg)
